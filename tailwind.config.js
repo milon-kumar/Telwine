d8a27d1549f54746ed0300eb86c89bb3c7a7d99a
milon-kumar <mk84124291@gmail.com>
2022-05-25 06:58:49 -0700

    inilije

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["dist/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'greatvibes': ['Great Vibes', 'cursive'],
+        'inter' : ['Inter', 'sans-serif'],
+        'roboto' : ['Roboto+Flex', 'sans-serif'],
+        'Macondo': ['Macondo', 'cursive']
+      }
+    },
+  },
+  plugins: [],
+}
